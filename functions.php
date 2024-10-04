407a16c1a840721f211cb028a6c9bc7ba6159c3e
Matthew Glen <matthewg1993@hotmail.co.uk>
2024-10-04 07:00:16 +0100

    first commit

---
+++ functions.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * This file adds functions to the Frost WordPress theme.
+ *
+ * @package Frost
+ * @author  WP Engine
+ * @license GNU General Public License v3
+ * @link    https://frostwp.com/
+ */
+
+if ( ! function_exists( 'frost_setup' ) ) {
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 *
+	 * @since 0.8.0
+	 *
+	 * @return void
+	 */
+	function frost_setup() {
+
+		// Make theme available for translation.
+		load_theme_textdomain( 'frost', get_template_directory() . '/languages' );
+
+		// Enqueue editor stylesheet.
+		add_editor_style( get_template_directory_uri() . '/style.css' );
+
+		// Remove core block patterns.
+		remove_theme_support( 'core-block-patterns' );
+
+	}
+}
+add_action( 'after_setup_theme', 'frost_setup' );
+
+// Enqueue stylesheet.
+add_action( 'wp_enqueue_scripts', 'frost_enqueue_stylesheet' );
+function frost_enqueue_stylesheet() {
+
+	wp_enqueue_style( 'frost', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'tailwind-css', get_template_directory_uri() . '/src/output.css' );
+}
+
+/**
+ * Register block styles.
+ *
+ * @since 0.9.2
+ */
+function frost_register_block_styles() {
+
+	$block_styles = array(
+		'core/columns' => array(
+			'columns-reverse' => __( 'Reverse', 'frost' ),
+		),
+		'core/group' => array(
+			'shadow-light' => __( 'Shadow', 'frost' ),
+			'shadow-solid' => __( 'Solid', 'frost' ),
+		),
+		'core/list' => array(
+			'no-disc' => __( 'No Disc', 'frost' ),
+		),
+		'core/quote' => array(
+			'shadow-light' => __( 'Shadow', 'frost' ),
+			'shadow-solid' => __( 'Solid', 'frost' ),
+		),
+		'core/social-links' => array(
+			'outline' => __( 'Outline', 'frost' ),
+		),
+	);
+
+	foreach ( $block_styles as $block => $styles ) {
+		foreach ( $styles as $style_name => $style_label ) {
+			register_block_style(
+				$block,
+				array(
+					'name'  => $style_name,
+					'label' => $style_label,
+				)
+			);
+		}
+	}
+}
+add_action( 'init', 'frost_register_block_styles' );
+
+/**
+ * Register block pattern categories.
+ *
+ * @since 1.0.4
+ */
+function frost_register_block_pattern_categories() {
+
+	register_block_pattern_category(
+		'frost-page',
+		array(
+			'label'       => __( 'Page', 'frost' ),
+			'description' => __( 'Create a full page with multiple patterns that are grouped together.', 'frost' ),
+		)
+	);
+	register_block_pattern_category(
+		'frost-pricing',
+		array(
+			'label'       => __( 'Pricing', 'frost' ),
+			'description' => __( 'Compare features for your digital products or service plans.', 'frost' ),
+		)
+	);
+
+}
+
+add_action( 'init', 'frost_register_block_pattern_categories' );
+
+// Disable comments for posts and pages
+function disable_comments_post_types_support() {
+	$post_types = get_post_types();
+	foreach ($post_types as $post_type) {
+			if (post_type_supports($post_type, 'comments')) {
+					remove_post_type_support($post_type, 'comments');
+					remove_post_type_support($post_type, 'trackbacks');
+			}
+	}
+}
+add_action('admin_init', 'disable_comments_post_types_support');
+
+// Close comments on the front-end
+function disable_comments_status() {
+	return false;
+}
+add_filter('comments_open', 'disable_comments_status', 20, 2);
+add_filter('pings_open', 'disable_comments_status', 20, 2);
+
+// Hide existing comments
+function disable_comments_hide_existing_comments($comments) {
+	return array();
+}
+add_filter('comments_array', 'disable_comments_hide_existing_comments', 10, 2);
+
+// Remove comments page in the menu
+function disable_comments_admin_menu() {
+	remove_menu_page('edit-comments.php');
+}
+add_action('admin_menu', 'disable_comments_admin_menu');
+
+// Redirect any user trying to access comments page
+function disable_comments_admin_menu_redirect() {
+	global $pagenow;
+	if ($pagenow === 'edit-comments.php') {
+			wp_redirect(admin_url()); exit;
+	}
+}
+add_action('admin_init', 'disable_comments_admin_menu_redirect');
+
+// Remove comments metabox from dashboard
+function disable_comments_dashboard() {
+	remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+}
+add_action('admin_init', 'disable_comments_dashboard');
+
+// Remove comments links from admin bar
+function disable_comments_admin_bar() {
+	if (is_admin_bar_showing()) {
+			remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+	}
+}
+add_action('init', 'disable_comments_admin_bar');
